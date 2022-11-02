944a22757a58ef3fa147474eb5996fa593336ed5
Tyler-Bell <113041440+Tyler-Bell@users.noreply.github.com>
2022-11-02 11:04:36 -0400

    lesson -05

--- code.py
+++ code.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python3
 
-# Created by: Evgeny Vovk
+# Created by: Tyler Bell
 # Created on: 2022 Oct
 # This program is the "Space Aliens" program on the PyBadge
 
 import ugame
 import stage
 
+import constants
+
+
 def game_scene():
     # this function is the main game game_scene
 
+    # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
+    # set the background to image 0 in the image bank
+    #   and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y -(2 * constants.SPRITE_SIZE))
 
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
     game.layers = [ship] + [background]
+    # render all sprites
+    #   most likely you will only render the background once per game scene
     game.render_block()
 
     while True:
---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Tyler Bell
+# Created on: 2022 Oct
+# This program is the "Space Aliens" program on the PyBadge
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS= 60
+SPRITE_MOVEMENT_SPEED = 1
